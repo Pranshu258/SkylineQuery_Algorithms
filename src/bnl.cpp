b2f10fff476e9d238f037e95fb293e9a5be59de0
Pranshu Gupta <pranshug258@outlook.com>
2016-09-11 18:14:26 +0530

    Fixed the miskate in checking for dominance in case of DVC

--- src/bnl.cpp
+++ src/bnl.cpp
@@ -13,191 +13,185 @@ using namespace std;
 struct point {
 	int* attributes;
 	double timestamp;
-    int index;
+	int index;
 };
 
 void stupid_print (list<point> data) {
-    for (list<point>::iterator p = data.begin(); p != data.end(); p++) {
-        cout << (*p).index << " ";
-    }
-    cout << endl;
-    return;
+	for (list<point>::iterator p = data.begin(); p != data.end(); p++) {
+		cout << (*p).index << " ";
+	}
+	cout << endl;
+	return;
 }
 
 int main(int argc, char *argv[]) {
-    
-    clock_t begin = clock();
+	
+	clock_t begin = clock();
 
-    // Input file: data.txt
-    ifstream infile("../data/data.txt");
-    int N, D, index, win_size;
-    infile >> N >> D;
+	// Input file: data.txt
+	ifstream infile("../data/data.txt");
+	int N, D, index, win_size;
+	infile >> N >> D;
 
-    ifstream infile1("../data/query.txt");
-    string line1, line2;
+	ifstream infile1("../data/query.txt");
+	string line1, line2;
 
-    // Read the Query Dimensions from the query.txt file
-    getline(infile1, line1);
-    stringstream ss1(line1);
-    vector<int> dims;
-    for(int i = 0; ss1 >> i; ) {
-        dims.push_back(i);
-    }
-    // Read the Window size from the query.txt file
-    getline(infile1, line2);
-    stringstream ss2(line2);
-    ss2 >> win_size;
+	// Read the Query Dimensions from the query.txt file
+	getline(infile1, line1);
+	stringstream ss1(line1);
+	vector<int> dims;
+	for(int i = 0; ss1 >> i; ) {
+		dims.push_back(i);
+	}
+	// Read the Window size from the query.txt file
+	getline(infile1, line2);
+	stringstream ss2(line2);
+	ss2 >> win_size;
 
-    // Read data from file and create the lst of data
-    list<point> data;
-    list<point> original_data;
-    bool *skyline = new bool[N];
+	// Read data from file and create the lst of data
+	list<point> data;
+	list<point> original_data;
+	bool *skyline = new bool[N];
 
-    for (int i = 0; i < N; ++i) {
-        infile >> index;
-        if (index == i+1) {
-            // Read the dimensions now and put them in the vector
-            point *p = new point;
-            p->attributes = new int[D];
-            for (int j = 0; j < D; j++) {
-                int val;
-                infile >> val;
-                p->attributes[j] = val;
-            }
-            p->timestamp = N*N;
-            p->index = index;
-            data.push_back(*p);
-            original_data.push_back(*p);
-        } else {
-            cout << "Invalid Data" << endl;
-            exit(0);    		
-        }
-    }
+	for (int i = 0; i < N; ++i) {
+		infile >> index;
+		if (index == i+1) {
+			// Read the dimensions now and put them in the vector
+			point *p = new point;
+			p->attributes = new int[D];
+			for (int j = 0; j < D; j++) {
+				int val;
+				infile >> val;
+				p->attributes[j] = val;
+			}
+			p->timestamp = N*N;
+			p->index = index;
+			data.push_back(*p);
+			original_data.push_back(*p);
+		} else {
+			cout << "Invalid Data" << endl;
+			exit(0);			
+		}
+	}
 
-    // Print the Data
-    // int c = 1;
-    // for (list<point>::iterator p = data.begin(); p != data.end(); p++) {
-    //     // *p contains the current point
-    //     cout << "[" << c << "]: "; 
-    //     for (vector<int>::iterator v = (*p).attributes.begin(); v != (*p).attributes.end(); v++) {
-    //         cout << *v << " ";
-    //     }
-    //     cout << endl;
-    //     c++;
-    // }
-
-
-    // BLOCK NESTED LOOP ALGORITHM FOR SKYLINES
-    //////////////////////////////////////////////////////////////////////////////
-    // NOW WE CAN START FINDING THE SKYLINES
-    int comparisons = 0;;
-    list<point> skyline_window;
-    while (!data.empty()) {
-        list<point> temp_data;
-
-        //cout << "Data: " << endl;
-        //stupid_print(data);
-
-        for (list<point>::iterator p = data.begin(); p != data.end(); p++) {
-
-            //cout << (*p).index << ". Skyline Window: " << endl;
-            //stupid_print(skyline_window);
-
-            bool not_skyline = false;
-            list<point>::iterator swp = skyline_window.begin();
-            while (swp != skyline_window.end()) {
-                int equalorworse = 0, worse = 0;
-                int equalorbetter = 0, better = 0;
-                for (vector<int>::iterator k = dims.begin(); k != dims.end(); ++k) {
-                    if ((*p).attributes[*k - 1] >= (*swp).attributes[*k - 1]) {
-                        equalorworse += 1;
-                        if ((*p).attributes[*k - 1] > (*swp).attributes[*k - 1]) {
-                            worse += 1;
-                        }
-                    } else if ((*p).attributes[*k - 1] <= (*swp).attributes[*k - 1]){
-                        equalorbetter += 1;
-                        if ((*p).attributes[*k - 1] < (*swp).attributes[*k - 1]) {
-                            better += 1;
-                        }
-                    }
-                }
-                if (equalorworse == dims.size() && worse > 0) {
-                    not_skyline = true;
-                }
-                if (equalorbetter == dims.size() && better > 0) {
-                    // remove swp from the window
-                    //cout << "Removed " << (*swp).index << " from Skyline window, dominated by: " << (*p).index << endl;
-                    swp = skyline_window.erase(swp);
-                    comparisons += 1;
-                    continue;
-                }
-                comparisons += 1; 
-                swp++;   
-            }
-            // if not_skyline is still false, try to insert this point in the skyline window
-            if (!not_skyline) {
-                if (skyline_window.size() < win_size) {
-                    // get the timestamp and push in the skyline data
-                    (*p).timestamp = comparisons;
-                    skyline_window.push_back(*p);
-                    //cout << "Inserted " << (*p).index << " in Skyline window. Time: " << (*p).timestamp << endl;
-                }
-                else {
-                    // get the timestamp and put in the temp data
-                    (*p).timestamp = comparisons;
-                    temp_data.push_back(*p);
-                    //cout << "Inserted " << (*p).index << " in temp_data. Time: " << (*p).timestamp << endl;
-                }
-            }
-        }
-
-        ////////////////////////////////////////////////////////////////////////////////////////////////////
-
-        // mark the skyline points
-        list<point>::iterator swp = skyline_window.begin();
-        while (swp != skyline_window.end()) {
-            //cout << "Size of Skyline Window: " << skyline_window.size() << endl;
-            if (skyline_window.size() > 0) {
-                if ((*swp).timestamp < (temp_data.front()).timestamp || temp_data.size() == 0) {
-                    //cout << "Removing " << swp->index << " from window as valid skyline" << endl;
-                    if (swp->index <= N) {
-                        skyline[(swp->index)-1] = true;
-                        swp = skyline_window.erase(swp);
-                        continue;
-                    } else {
-                        cout << "Something went wrong" << endl;
-                    }
-                } else {
-                    break;
-                }
-            } else {
-                break;
-            }
-            swp++;
-        }        
-
-        //cout << "-------------------------------------------" << endl;
-
-        data = temp_data;
-        temp_data.clear();
-    }
+	// Print the Data
+	// int c = 1;
+	// for (list<point>::iterator p = data.begin(); p != data.end(); p++) {
+	//	 // *p contains the current point
+	//	 cout << "[" << c << "]: "; 
+	//	 for (vector<int>::iterator v = (*p).attributes.begin(); v != (*p).attributes.end(); v++) {
+	//		 cout << *v << " ";
+	//	 }
+	//	 cout << endl;
+	//	 c++;
+	// }
 
 
-    /////////////////////////////////////////////////////////////////////////////
+	// BLOCK NESTED LOOP ALGORITHM FOR SKYLINES
+	//////////////////////////////////////////////////////////////////////////////
+	// NOW WE CAN START FINDING THE SKYLINES
+	int comparisons = 0;;
+	list<point> skyline_window;
+	while (!data.empty()) {
+		list<point> temp_data;
 
-    cout << "Skyline Points: " << endl;
-    int printed = 0;
-    for (int i = 0; i < N; i++) {
-        if (skyline[i]) {
-            cout << i+1 << "\t";
-            printed += 1;
-            if (printed % 10 == 0)
-                cout << endl;
-        }
-    }
-    cout << endl;
-    cout << "Number of skyline points: " << printed << endl;
-    cout << "Number of comparisons: " << comparisons << endl;
+		//cout << "Data: " << endl;
+		//stupid_print(data);
 
-    return 0;
+		for (list<point>::iterator p = data.begin(); p != data.end(); p++) {
+			bool not_skyline = false;
+			list<point>::iterator swp = skyline_window.begin();
+			while (swp != skyline_window.end()) {
+				int equalorworse = 0, worse = 0;
+				int equalorbetter = 0, better = 0;
+				for (vector<int>::iterator k = dims.begin(); k != dims.end(); ++k) {
+					if ((*p).attributes[*k - 1] > (*swp).attributes[*k - 1]) {
+						worse += 1;
+					} else if ((*p).attributes[*k - 1] == (*swp).attributes[*k - 1]) {
+						equalorworse += 1;
+						equalorbetter += 1;
+					} else if ((*p).attributes[*k - 1] < (*swp).attributes[*k - 1]) {
+						better += 1;
+					}
+				}
+				if (equalorworse + worse == dims.size() && worse > 0) {
+					not_skyline = true;
+				}
+				if (equalorbetter + better == dims.size() && better > 0) {
+					// remove swp from the window
+					//cout << "Removed " << (*swp).index << " from Skyline window, dominated by: " << (*p).index << endl;
+					swp = skyline_window.erase(swp);
+					comparisons += 1;
+					continue;
+				}
+				comparisons += 1; 
+				swp++;
+			}				
+
+			// if not_skyline is still false, try to insert this point in the skyline window
+			if (!not_skyline) {
+				if (skyline_window.size() < win_size) {
+					// get the timestamp and push in the skyline data
+					(*p).timestamp = comparisons;
+					skyline_window.push_back(*p);
+					//cout << "Inserted " << (*p).index << " in Skyline window. Time: " << (*p).timestamp << endl;
+				}
+				else {
+					// get the timestamp and put in the temp data
+					(*p).timestamp = comparisons;
+					temp_data.push_back(*p);
+					//cout << "Inserted " << (*p).index << " in temp_data. Time: " << (*p).timestamp << endl;
+				}
+			}
+		}
+
+		////////////////////////////////////////////////////////////////////////////////////////////////////
+
+		// mark the skyline points
+		list<point>::iterator swp = skyline_window.begin();
+		while (swp != skyline_window.end()) {
+			//cout << "Size of Skyline Window: " << skyline_window.size() << endl;
+			if (skyline_window.size() > 0) {
+				if ((*swp).timestamp < (temp_data.front()).timestamp || temp_data.size() == 0) {
+					//cout << "Removing " << swp->index << " from window as valid skyline" << endl;
+					if (swp->index <= N) {
+						skyline[(swp->index)-1] = true;
+						swp = skyline_window.erase(swp);
+						continue;
+					} else {
+						cout << "Something went wrong" << endl;
+					}
+				} else {
+					break;
+				}
+			} else {
+				break;
+			}
+			swp++;
+		}		
+
+		//cout << "-------------------------------------------" << endl;
+
+		data = temp_data;
+		temp_data.clear();
+	}
+
+
+	/////////////////////////////////////////////////////////////////////////////
+
+	cout << "Skyline Points: " << endl;
+	int printed = 0;
+	for (int i = 0; i < N; i++) {
+		if (skyline[i]) {
+			cout << i+1 << "\t";
+			printed += 1;
+			if (printed % 10 == 0)
+				cout << endl;
+		}
+	}
+	cout << endl;
+	cout << "Number of skyline points: " << printed << endl;
+	cout << "Number of comparisons: " << comparisons << endl;
+
+	return 0;
 }
--- src/naive.cpp
+++ src/naive.cpp
@@ -11,81 +11,81 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
-    // Input file: data.txt
-    ifstream infile("../data/data.txt");
-    int N, D, index;
-    infile >> N >> D;
+	// Input file: data.txt
+	ifstream infile("../data/data.txt");
+	int N, D, index;
+	infile >> N >> D;
 
-    // Read the Query from the query.txt file
-    string line1;
-    ifstream infile1("../data/query.txt");
-    getline(infile1, line1);
-    stringstream ss1(line1);
-    vector<int> dims;
-    for(int i = 0; ss1 >> i; ) {
-        dims.push_back(i);
-    }
+	// Read the Query from the query.txt file
+	string line1;
+	ifstream infile1("../data/query.txt");
+	getline(infile1, line1);
+	stringstream ss1(line1);
+	vector<int> dims;
+	for(int i = 0; ss1 >> i; ) {
+		dims.push_back(i);
+	}
 
-    // Create a Matrix for the data
-    int **data = new int*[N];
-    bool *not_skyline = new bool[N];
+	// Create a Matrix for the data
+	int **data = new int*[N];
+	bool *not_skyline = new bool[N];
 
-    for (int i = 0; i < N; i++) {
-        infile >> index;
-        if (index == i+1) {
-            data[i] = new int[D];
-            for (int j = 0; j < D; j++) {
-                infile >> data[i][j];
-            }
-        } else {
-            cout << "Invalid Data" << endl;
-            exit(0);
-        }
-    }
+	for (int i = 0; i < N; i++) {
+		infile >> index;
+		if (index == i+1) {
+			data[i] = new int[D];
+			for (int j = 0; j < D; j++) {
+				infile >> data[i][j];
+			}
+		} else {
+			cout << "Invalid Data" << endl;
+			exit(0);
+		}
+	}
 
-    // NAIVE ALGORITHM FOR SKYLINES
-    ////////////////////////////////////////////////////////////////////////////////////////
-    // Now let's find out the skylines in the dataset
-    int number_skylines = 0;
-    for (int i = 0; i < N; i++) {
-        // Compare data[i] with all the other datapoints
-        not_skyline[i] = false;
-        for (int j = 0; j < N; j++) {
-            int worse = 0, equalorworse = 0;
-            for (vector<int>::iterator k = dims.begin(); k != dims.end(); ++k) {
-                if (data[i][*k - 1] > data[j][*k - 1]) {
-                    worse += 1;
-                }
-                if (data[i][*k - 1] >= data[j][*k - 1]) {
-                    equalorworse += 1;
-                }
-            }
-            if (equalorworse == dims.size() && worse > 0) {
-                not_skyline[i] = true;
-                // cout << "[" << i+1 << "] beaten by [" << j+1 << "]" << endl;
-                break;
-            }
-        }
-        if (!not_skyline[i]) {
-            // This point was not dominated by any other point, so it is a skyline
-            // cout << "[" << i+1 << "] is a Skyline" << endl; 
-            number_skylines += 1;  
-        }
-    }
-    ////////////////////////////////////////////////////////////////////////////////////////
+	// NAIVE ALGORITHM FOR SKYLINES
+	////////////////////////////////////////////////////////////////////////////////////////
+	// Now let's find out the skylines in the dataset
+	int number_skylines = 0;
+	for (int i = 0; i < N; i++) {
+		// Compare data[i] with all the other datapoints
+		not_skyline[i] = false;
+		for (int j = 0; j < N; j++) {
+			int worse = 0, equalorworse = 0;
+			for (vector<int>::iterator k = dims.begin(); k != dims.end(); ++k) {
+				if (data[i][*k - 1] > data[j][*k - 1]) {
+					worse += 1;
+				}
+				if (data[i][*k - 1] >= data[j][*k - 1]) {
+					equalorworse += 1;
+				}
+			}
+			if (equalorworse == dims.size() && worse > 0) {
+				not_skyline[i] = true;
+				// cout << "[" << i+1 << "] beaten by [" << j+1 << "]" << endl;
+				break;
+			}
+		}
+		if (!not_skyline[i]) {
+			// This point was not dominated by any other point, so it is a skyline
+			// cout << "[" << i+1 << "] is a Skyline" << endl; 
+			number_skylines += 1;  
+		}
+	}
+	////////////////////////////////////////////////////////////////////////////////////////
 
-    cout << "Skyline Points: " << endl;
-    int printed = 0;
-    for (int i = 0; i < N; i++) {
-        if (!not_skyline[i]) {
-            cout << i+1 << "\t";
-            printed += 1;
-            if (printed % 10 == 0)
-                cout << endl;
-        }
-    }
-    cout << endl;
-    cout << "Number of skyline points: " << printed << endl;
-    cout << "Number of comparisons: " << N*N << endl;
-    return 0;
+	cout << "Skyline Points: " << endl;
+	int printed = 0;
+	for (int i = 0; i < N; i++) {
+		if (!not_skyline[i]) {
+			cout << i+1 << "\t";
+			printed += 1;
+			if (printed % 10 == 0)
+				cout << endl;
+		}
+	}
+	cout << endl;
+	cout << "Number of skyline points: " << printed << endl;
+	cout << "Number of comparisons: " << N*N << endl;
+	return 0;
 }
